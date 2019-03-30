d3589fa48c1763b50087cdbb2b643b7b4c40391b
faog <fabiola.orellana.g@gmail.com>
2019-03-30 14:46:17 -0300

    cambio nombre index.html

--- src/main.js
+++ src/main.js
@@ -11,24 +11,26 @@ var db = firebase.firestore();
 
 
 //Funcion boton guardar publicación en la base de datos
-document.getElementById('messageadd').addEventListener('click',()=>{
- let messageInput=document.getElementById("message").value;
- let situacionSelect = document.getElementById("situacion").value;
+if(document.getElementById('messageadd')){
+    document.getElementById('messageadd').addEventListener('click',()=>{
+    let messageInput=document.getElementById("message").value;
+    let situacionSelect = document.getElementById("situacion").value;
 
-    db.collection("users").add({
-        message: messageInput,
-        situacion: situacionSelect
-    
+        db.collection("users").add({
+            message: messageInput,
+            situacion: situacionSelect
+        
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            document.getElementById("message").value='';
+            document.getElementById("situacion").value='all';
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
     })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        document.getElementById("message").value='';
-        document.getElementById("situacion").value='all';
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-})
+}
 
 //funcion postear informacion en el muro (leer de la base de datos)
 let containerMessage=document.getElementById("result");
@@ -39,6 +41,22 @@ db.collection("users").onSnapshot((querySnapshot) => {
         containerMessage.innerHTML +=
         `<h3>post</h3>
         <h6>${doc.data().message}</h6>
-        <h6>"#"${doc.data().situacion}</h6>`
+        <h6>"#"${doc.data().situacion}</h6>
+        <!--<button id="delete">Eliminar</button>-->`
     });
 });
+
+//Funcion para borrar un dato
+/*if(document.getElementById('delete')){
+    document.getElementById('delete').addEventListener('click',()=>{
+        let id= document.getElementById(${doc.id).value;
+        db.collection("users").doc(id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    })
+} */
+
+
+
